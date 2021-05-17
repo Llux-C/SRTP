ba3fe49cc7d7b2c533552635c567daff532ce361
Llux-C <244421824@qq.com>
2021-05-17 22:10:47 +0800

    qaq

--- src/components/A3.vue
+++ src/components/A3.vue
@@ -1,18 +1,112 @@
 <template>
-<div>
-<h1 style="font-size:20px;margin-top:20px;margin-left:40px">图形展示</h1>
-<div id="main" style="width: 600px;height:400px;"></div>
-</div>
+  <div>
+    <h1 style="font-size: 20px; margin-top: 20px; margin-left: 40px">
+      图形展示
+    </h1>
+    <div id="chart" style="width: 100%; height: 400px;"></div>
+  </div>
 </template>
 
 <script>
+import * as echarts from "echarts";
 
 export default {
   data() {
-    return {
-    }
+    return {};
   },
   methods: {
+    drawChart() {
+      var chartDom = document.getElementById("chart");
+      var myChart = echarts.init(chartDom);
+      var option;
+
+      function randomData() {
+        now = new Date(+now + oneDay);
+        value = value + Math.random() * 21 - 10;
+        return {
+          name: now.toString(),
+          value: [
+            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join("/"),
+            Math.round(value),
+          ],
+        };
+      }
+
+      var data = [];
+      var now = +new Date(1997, 9, 3);
+      var oneDay = 24 * 3600 * 1000;
+      var value = Math.random() * 1000;
+      for (var i = 0; i < 1000; i++) {
+        data.push(randomData());
+      }
+
+      option = {
+        title: {
+          text: "",
+        },
+        tooltip: {
+          trigger: "axis",
+          formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return (
+              date.getDate() +
+              "/" +
+              (date.getMonth() + 1) +
+              "/" +
+              date.getFullYear() +
+              " : " +
+              params.value[1]
+            );
+          },
+          axisPointer: {
+            animation: false,
+          },
+        },
+        xAxis: {
+          type: "time",
+          splitLine: {
+            show: false,
+          },
+        },
+        yAxis: {
+          type: "value",
+          boundaryGap: [0, "100%"],
+          splitLine: {
+            show: false,
+          },
+        },
+        series: [
+          {
+            name: "模拟数据",
+            type: "line",
+            showSymbol: false,
+            hoverAnimation: false,
+            data: data,
+          },
+        ],
+      };
+
+      setInterval(function () {
+        for (var i = 0; i < 5; i++) {
+          data.shift();
+          data.push(randomData());
+        }
+
+        myChart.setOption({
+          series: [
+            {
+              data: data,
+            },
+          ],
+        });
+      }, 1000);
+
+      option && myChart.setOption(option);
+    },
+  },
+  mounted: function () {
+    this.drawChart();
   },
 };
 </script>
--- src/views/Calculate.vue
+++ src/views/Calculate.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="calculate" style="minHeight:calc(100vh - 139px)">
-      <A1 />
+  <div class="check" style="minHeight:calc(100vh - 139px)">
+      <a-row style="minHeight:300px">
+          <a-col :span="12">
+            <A1 />
+          </a-col>
+          <a-col :span="12">
+            <A2 />
+          </a-col>
+      </a-row>
   </div>
 </template>
 
 <script>
-
 import A1 from '../components/A1'
+import A2 from '../components/A2'
 
-export default {
-  name: 'calculate',
-  components: {
-    'A1':A1,
+export default{
+    name: 'calculate',
+    components: {
+        'A1':A1,
+        'A2':A2,
   }
 }
 </script>
 
+
 <style scoped>
-.calculate{
-  text-align: center;
+.check{
   background-color: white;
 }
 </style>
